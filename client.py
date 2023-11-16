1117da190a57f5e09f8922a25e960fcb90ae3920
Chiara Romanini <caromanini@miuandes.cl>
2023-11-16 03:08:13 -0300

    create/take tests

--- client.py
+++ client.py
@@ -1,29 +1,131 @@
 import socket
+import json
+import logging
+
+BUFFER_SIZE = 1024
+SERVER_ADDRESS = ('127.0.0.1', 12345)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def list_tests(client):
+    client.send("LIST_TESTS".encode())
+    test_list_json = client.recv(BUFFER_SIZE).decode()
+    test_list = json.loads(test_list_json)
+    return test_list
+
+def choose_test(client):
+    test_list = list_tests(client)
+    if not test_list:
+        print("No tests available.")
+        return None
+
+    print("Available Tests:")
+    for i, test_name in enumerate(test_list, start=1):
+        print(f"{i}. {test_name}")
+
+    while True:
+        try:
+            choice = int(input("Choose a test (enter the corresponding number): "))
+            if 1 <= choice <= len(test_list):
+                return test_list[choice - 1]
+            else:
+                print("Invalid choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 12345))
+    client.connect(SERVER_ADDRESS)
 
-    score = 0
+    terminate_client = False
 
-    while True:
-        question = client.recv(1024).decode()
-        if not question:
-            break
+    try:
+        while not terminate_client:
+            username = input("Enter username (@miuandes.cl): ")
+            client.send(username.encode())
 
-        options = client.recv(1024).decode()       
-        print(f"{question}\n{options}")
+            username_check = client.recv(BUFFER_SIZE).decode()
 
-        if options == "END_OF_TEST":
-            print("Prueba completada. Saliendo del programa.")
-            break
+            if username_check != "AUTHENTICATION_VERIFIED":
+                logger.warning("Authentication failed.")
+                client.close()
+                break
 
-        response = input("Your answer: ").upper()
-        client.send(response.encode())
+            print("Options: ")
+            print("1. Create test")
+            print("2. Take test")
 
-    final_score = client.recv(1024).decode()
-    print(final_score)
-    client.close()
+            opciones_cliente = input("Choose option: ")
+            client.send(opciones_cliente.encode())
+
+            if opciones_cliente == "1":
+                client.send("CREATE_TEST".encode())
+                create_test(client)
+
+            elif opciones_cliente == "2":
+                test_name = choose_test(client)
+                client.send(test_name.encode())
+
+                try:
+                    test_start = client.recv(1024).decode()
+                    
+                    if test_start == "TEST_START":
+                        while True:
+                            question = client.recv(1024).decode()
+                            options = client.recv(1024).decode()
+
+                            if not question:
+                                break
+
+                            print(f"{question}\n{options}")
+
+                            if options == "END_OF_TEST":
+                                print("Test completed")
+                                break
+                            
+                            response = input("Your answer: ").upper()
+                            client.send(response.encode())
+
+                except ConnectionAbortedError:
+                    print("Connection with the server has been terminated.")
+                    terminate_client = True
+
+            else:
+                logger.warning("Invalid option. Please enter a valid option.")
+
+        logger.info("Client terminated.")  
+
+    finally:
+        client.close()
+
+def create_test(client):
+    test_name = input("Enter the name for the test: ")
+    client.send(test_name.encode())
+
+    num_questions = int(input("Enter the number of questions for the test: "))
+    for i in range(1, num_questions + 1):
+        question = input(f"Enter the question for question {i}: ")
+        options = [input(f"Enter option {j} for question {i}: ") for j in ["A", "B", "C", "D"]]
+        correct_answer = input(f"Enter the correct answer (A, B, C or D) for question {i}: ").upper()
+
+        question_data = {
+            "question": question,
+            "options": options,
+            "correct_answer": correct_answer
+        }
+
+        client.send(json.dumps(question_data).encode())
+
+        ack = client.recv(BUFFER_SIZE).decode()
+        if ack != "ACK":
+            logger.error("Error: Server did not acknowledge the data. Test creation failed.")
+            return
+
+    client.send("END_OF_TEST_CREATION".encode())
+
+    print("Test created successfully")
 
 if __name__ == "__main__":
     start_client()
--- server.py
+++ server.py
@@ -1,71 +1,150 @@
 import socket
 import threading
 import json
+import logging
+import os
 
-def handle_client(client_socket):
-    test_data = load_test()
+BUFFER_SIZE = 1024
+SERVER_ADDRESS = ('0.0.0.0', 12345)
 
-    if test_data is None:
-        print("[*] No hay pruebas disponibles. Se entregará una prueba estandar.")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-        test_data = {
-            "question1": {
-                "question": "Capital de Chile?",
-                "options": ["A. London", "B. Paris", "C. Santiago", "D. Madrid"],
-                "correct_answer": "C"
-            },
-            "question2": {
-                "question": "Que año es?",
-                "options": ["A. 2012", "B. 2023", "C. 1999", "D. 1800"],
-                "correct_answer": "B"
-            }
-        }
-
-    score = 0
-
-    for _, question_data in test_data.items():
-        question = question_data["question"]
-        options = "\n".join(question_data["options"])
-
-        client_socket.send(question.encode())
-        client_socket.send(options.encode())
-
-        response = client_socket.recv(1024).decode().upper()
-        correct_answer = question_data["correct_answer"]
-
-        if response == correct_answer:
-            score += 1
-
-    client_socket.send(f"Your score: {score}/{len(test_data)}".encode())
-    client_socket.send("END_OF_TEST".encode())
-
-    client_socket.close()
-
-def load_test():
+def list_tests():
     try:
-        with open('test.json', 'r') as file:
+        test_files = [file for file in os.listdir() if file.endswith(".json")]
+
+        return test_files
+    except Exception as e:
+        logger.error(f"Error listing tests: {e}")
+        return []
+    
+def send_test_list(client_socket):
+    test_list = list_tests()
+    if test_list:
+        client_socket.send(json.dumps(test_list).encode())
+    else:
+        client_socket.send("NO_TESTS_AVAILABLE".encode())
+
+
+def take_test(client_socket, test_name):
+    try:
+        test_data = load_test(test_name)
+
+        if test_data is None:
+            client_socket.send("NO_TEST_FOUND".encode())
+            return
+
+        client_socket.send("TEST_START".encode())
+
+        score = 0
+
+        for _, question_data in test_data.items():
+            question = question_data["question"]
+            options = "\n".join(question_data["options"])
+
+            client_socket.send(question.encode())
+            client_socket.send(options.encode())
+
+            response = client_socket.recv(BUFFER_SIZE).decode().upper()
+            correct_answer = question_data["correct_answer"]
+
+            if response == correct_answer:
+                score += 1
+
+        client_socket.send(f"Your score: {score}/{len(test_data)}".encode())
+        client_socket.send("END_OF_TEST".encode())
+
+    except Exception as e:
+        logger.error(f"Error taking test: {e}")
+
+def handle_client(client_socket, lock):
+    try:
+        username = client_socket.recv(BUFFER_SIZE).decode()
+
+        if "@miuandes.cl" in username:  # AUTENTICAR USUARIO
+            client_socket.send("AUTHENTICATION_VERIFIED".encode())
+        else:
+            logger.warning("Invalid username format. Closing connection.")
+            return
+
+        respuesta_cliente = client_socket.recv(BUFFER_SIZE).decode()
+
+        if respuesta_cliente == "1":
+            prueba = client_socket.recv(BUFFER_SIZE).decode()
+
+            if prueba == "CREATE_TEST":
+                create_test(client_socket, lock)
+
+        elif respuesta_cliente == "2":
+            send_test_list(client_socket)
+            aux = client_socket.recv(BUFFER_SIZE).decode()
+            test_choice = client_socket.recv(BUFFER_SIZE).decode()
+
+            if test_choice in list_tests():
+                take_test(client_socket, test_choice)
+            else:
+                client_socket.send("INVALID_TEST_CHOICE".encode())
+
+        elif respuesta_cliente == "FINISH":
+            logger.info("Terminating connection.")
+        else:
+            logger.warning("Invalid client response.")
+        
+    except Exception as e:
+        logger.error(f"Error handling client: {e}")
+
+    finally:
+        client_socket.close()
+
+def create_test(client_socket, lock):
+    try:
+        test_name = client_socket.recv(BUFFER_SIZE).decode()
+        test_data = {}
+
+        while True:
+            question_json = client_socket.recv(BUFFER_SIZE).decode()
+            if question_json == "END_OF_TEST_CREATION":
+                break
+
+            client_socket.send("ACK".encode())
+
+            question_data = json.loads(question_json)
+            with lock:
+                test_data[f"question{len(test_data) + 1}"] = question_data
+
+        save_test(test_name, test_data)
+        logger.info(f"Test '{test_name}' created successfully")
+
+    except Exception as e:
+        logger.error(f"Error creating test: {e}")
+
+def load_test(test_name):
+    try:
+        with open(f'{test_name}', 'r') as file:
             test_data = json.load(file)
         return test_data
-    
+
     except FileNotFoundError:
         return None
-    
-def save_test(test_data):
-    with open('test.json', 'w') as file:
-        json.dump(test_data, file)
 
+def save_test(test_name, test_data):
+    with open(f'{test_name}.json', 'w') as file:
+        json.dump(test_data, file)
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 12345))
+    server.bind(SERVER_ADDRESS)
     server.listen(5)
-    print("[*] Server listening on port 12345")
+    logger.info("[*] Server listening on port 12345")
+
+    lock = threading.Lock()
 
     while True:
         client, addr = server.accept()
-        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
+        logger.info(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
 
-        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler = threading.Thread(target=handle_client, args=(client, lock))
         client_handler.start()
 
 if __name__ == "__main__":
